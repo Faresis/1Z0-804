020109134e3b107d42914b7e41833a70af1766ff
Faresis <nikignatiev@gmail.com>
2014-10-22 08:58:24 +0300

    Multibound generics example

--- src/ua/dp/mign/generics/bounded/MultiBound.java
+++ src/ua/dp/mign/generics/bounded/MultiBound.java
@@ -10,23 +10,46 @@ interface Processor {
 
 class Person {
     int age;
-    int heigh;
+    int height;
+    Person(int age, int height) {
+        this.age = age;
+        this.height = height;
+    }
+}
+
+class Employee extends Person implements Processor, Worker {
+    Employee(int age, int height) {
+        super(age, height);
+    }
+    public void doWork() {
+        System.out.println("Employee is working");
+    }
+    public void process(Object o) {
+        System.out.println("Employee processing " + o);
+    }
 }
 
 class MultiBound {
-    public static <T extends Number> void process(T obj) {
-        System.out.println(obj.doubleValue());
+    /*
+     * With multiple bounds members of all mentioned types are available
+     * inside a method: age & height from Person, process from Processor
+     * and doWork from Worker.
+     */
+    public static <T extends Person & Worker & Processor> void test(T obj) {
+        System.out.println("Age: " + obj.age);
+        System.out.println("Height: " + obj.height);
+        obj.process("brick");
+        obj.doWork();
     }
 
     public static void main(String[] args) {
-        process(new Integer(45));
-        process(new Double(46));
-        process(new Float(47));
+        test(new Employee(25, 180));
 
         /*
          * Will not compile since bound condition is not satisfied.
          */
-        //process(new Object());
-        //process("hello");
+        //test(new Person(25, 180));
+        //test(new Object());
+        //test("hello");
     }
 }
