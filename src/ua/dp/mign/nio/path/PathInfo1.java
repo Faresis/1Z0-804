2ae6a231f789bf43cc0d0dacad4890f7d3af8e2b
Faresis <nikignatiev@gmail.com>
2014-12-07 13:44:54 +0200

    Path information extended example

--- src/ua/dp/mign/nio/path/PathInfo1.java
+++ src/ua/dp/mign/nio/path/PathInfo1.java
@@ -3,6 +3,8 @@ package ua.dp.mign.nio.path;
 import java.io.Console;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.nio.file.LinkOption;
+import java.io.IOException;
 
 class PathInfo1 {
     public static void main(String args[]) {
@@ -23,10 +25,19 @@ class PathInfo1 {
         System.out.println("File name: " + testFilePath.getFileName());
         System.out.println("Root: " + testFilePath.getRoot());
         System.out.println("Parent: " + testFilePath.getParent());
+        System.out.println("URI: " + testFilePath.toUri());
+        System.out.println("Absolute path: " + testFilePath.toAbsolutePath());
+        System.out.println("Normalized path: " + testFilePath.normalize());
+        System.out.println("Normalized absolute path: " + testFilePath.normalize().toAbsolutePath());
 
         System.out.println("Printing elements of the path: ");
         for(Path element : testFilePath) {
             System.out.println("Path element: " + element);
+        }
+        try {
+            System.out.println("Real path: " + testFilePath.toRealPath(LinkOption.NOFOLLOW_LINKS));
+        } catch (IOException ioe) {
+            System.err.printf("Error during processing file %s.", testFilePath);
         }
     }
 }
