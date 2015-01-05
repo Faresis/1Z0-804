88c3fdfad7b6a5afa0841d333cc49a21f2f781ba
Faresis <nikignatiev@gmail.com>
2015-01-05 22:01:09 +0200

    ResourceBundle.Control example

--- src/ua/dp/mign/locale/LocalizedHello.java
+++ src/ua/dp/mign/locale/LocalizedHello.java
@@ -1,12 +1,42 @@
 package ua.dp.mign.locale;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+class PrintResourceBundleControl extends ResourceBundle.Control {
+    public List<Locale> getCandidateLocales(String baseName, Locale locale) {
+        List<Locale> candidates = super.getCandidateLocales(baseName, locale);
+        System.out.printf("Candidate locales for the base name %s and locale %s %n", baseName, locale.getDisplayName());
+        for(Locale candidate : candidates) {
+            System.out.println(candidate);
+        }
+        return candidates;
+    }
+}
+
 class LocalizedHello {
     public static void main(String[] args) {
-        Locale currentLocale = Locale.getDefault();
-        ResourceBundle bundle = ResourceBundle.getBundle("GreetingBundle", currentLocale);
+        ResourceBundle bundle = getBundle();
         System.out.println(bundle.getString("Greeting"));
+    }
+
+    private static ResourceBundle getBundle() {
+        /*
+         * System will first search for localized bundle for the
+         * locale set programmatically.
+         * If no matches it will continue to search with a default
+         * locale.
+         * And finally search for a pure bundle name will be performed. 
+         */
+        Locale currentLocale = new Locale("fr","CA");
+        ResourceBundle res = ResourceBundle.getBundle("GreetingBundle", currentLocale, new PrintResourceBundleControl());
+        String locale = res.getLocale().toString();
+        if("".equals(locale)) {
+            System.out.println("Loaded default property file: GreetingBundle."); 
+        } else {
+            System.out.printf("Loaded the resource bundle for the locale: GreetingBundle_%s%n", locale);
+        }
+        return res;
     }
 } 
