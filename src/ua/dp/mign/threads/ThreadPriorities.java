6bc28c9ab54e2c93bfb42ae76d1a9b391258010d
Faresis <nikignatiev@gmail.com>
2015-01-08 08:28:22 +0200

    Thread priorities example

---
+++ src/ua/dp/mign/threads/ThreadPriorities.java
@@ -0,0 +1,9 @@
+package ua.dp.mign.threads;
+
+class ThreadPriorities {
+    public static void main(String[] args) {
+        System.out.printf("Minimum priority of a thread: %s.%n", Thread.MIN_PRIORITY);
+        System.out.printf("Normal priority of a thread: %s.%n", Thread.NORM_PRIORITY);
+        System.out.printf("Maximum priority of a thread: %s.%n", Thread.MAX_PRIORITY);
+    }
+}
